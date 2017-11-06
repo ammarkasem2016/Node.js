0a34fef219b9351f31c554707f9afbfd3fc1193d
ammarkasem2016 <ammarkasem2016@gmail.com>
2017-11-06 12:25:50 +0100

    Ammar

--- week1/index.js
+++ week1/index.js
@@ -1,38 +1,25 @@
-import HTTP from 'http'
-import Path from 'path'
-
-import sendIndexHTML from './responses/sendIndexHTML'
-import sendPage2HTML from './responses/sendPage2HTML'
-import sendStylesCSS from './responses/sendStylesCSS'
-import sendText from './responses/sendText'
-
-const server = HTTP.createServer((request, response) => {
-	console.log(request.method, request.url)
-
-	switch (request.url) {
-	case '/':
-		sendIndexHTML(response)
-		break
-	case '/page2':
-		sendPage2HTML(response)
-		break
-	case '/styles.css':
-		sendStylesCSS(response)
-		break
-	default:
-		const extension = Path.extname(request.url)
-		if (extension === '') {
-			response.statusCode = 302
-			response.setHeader('Location', '/')
-		} else {
-			response.statusCode = 404
-			sendText(response, "File not found")
-		}
-	}
-	
-	response.end()
-})
-
-server.listen(3001)
-
-console.log('Server started')
+var state = 10;
+const http = require('http');
+const port = 3000;
+const server = http.createServer((request, response) => {
+    if (request.url === "/state") {
+        response.write(String(state));
+    } else if (request.url === "/add") {
+        state++;
+        response.write(String(state));
+    } else if (request.url === "/remove") {
+        state--;
+        response.write(String(state));
+    } else if (request.url === "/reset") {
+        state = 10;
+        response.write(String(state));
+    } else {
+        response.writeHead(404, {
+            'Content-Type': 'text/html'
+        });
+    }
+    response.end();
+});
+server.listen(port, () => {
+    console.log(`The nodejs server is now listening on ${port}`);
+});
